268d52445ed753578488a585a393099ff4fd199d
Levan Ilashvili <levan@ilashvili.me>
2024-11-17 00:03:17 +0400

    hardhat config setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,12 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: process.env.NODE_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
